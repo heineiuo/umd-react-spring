01eaab9619ef125ec2c7a4bd566dc0732857bb48
heineiuo <heineiuo@gmail.com>
2020-09-13 17:18:15 +0800

    First commit

---
+++ scripts/create-entry.js
@@ -0,0 +1,26 @@
+const fs = require("fs").promises;
+const path = require("path");
+const yargs = require("yargs");
+
+const argv = yargs.argv;
+
+async function createEntry() {
+  try {
+    try {
+      await fs.mkdir(path.resolve(process.cwd(), "./src"));
+    } catch (e) {}
+    const content = `import Default from '${argv.pkg}';
+export default Default;`;
+    await fs.writeFile(
+      path.resolve(process.cwd(), "./src/index.ts"),
+      content,
+      "utf8"
+    );
+    console.log("Create entry file success");
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+createEntry();
